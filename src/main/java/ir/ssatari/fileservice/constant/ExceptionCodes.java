9b816670eca58822f27e75ad2c05db43b7c865a7
SaeedSatari <saeedsatari93@gmail.com>
2019-05-28 17:54:42 +0430

    added exception messages

---
+++ src/main/java/ir/ssatari/fileservice/constant/ExceptionCodes.java
@@ -0,0 +1,9 @@
+package ir.ssatari.fileservice.constant;
+
+public class ExceptionCodes {
+    private ExceptionCodes() {
+    }
+    public static final String FILE_SRV_COULD_NOT_CREATE_DIRECTORY = "Could not create the directory where the uploaded files will be stored.";
+    public static final String FILE_SRV_INVALID_FILE_NAME = "Sorry! Filename contains invalid path sequence ";
+    public static final String FILE_SRV_NOT_FOUND = "File not found ";
+}
