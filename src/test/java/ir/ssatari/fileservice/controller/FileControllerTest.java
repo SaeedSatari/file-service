4909159c22e8c7070ae31924a5637f150b328daf
SaeedSatari <saeedsatari93@gmail.com>
2019-05-28 18:23:51 +0430

    added jupiter api

---
+++ src/test/java/ir/ssatari/fileservice/controller/FileControllerTest.java
@@ -0,0 +1,20 @@
+package ir.ssatari.fileservice.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FileControllerTest {
+
+    @Test
+    void uploadFile() {
+    }
+
+    @Test
+    void uploadMultipleFiles() {
+    }
+
+    @Test
+    void downloadFile() {
+    }
+}
---
+++ src/test/java/ir/ssatari/fileservice/service/FileStorageServiceTest.java
@@ -0,0 +1,16 @@
+package ir.ssatari.fileservice.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FileStorageServiceTest {
+
+    @Test
+    void storeFile() {
+    }
+
+    @Test
+    void loadFileAsResource() {
+    }
+}
