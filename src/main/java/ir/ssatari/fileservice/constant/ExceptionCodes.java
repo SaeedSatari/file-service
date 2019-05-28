a52bdcca1c2c79826175e8cc5fbaedb62a1e3671
SaeedSatari <saeedsatari93@gmail.com>
2019-05-28 18:07:37 +0430

    implemented download file endpoint

--- src/main/java/ir/ssatari/fileservice/constant/ExceptionCodes.java
+++ src/main/java/ir/ssatari/fileservice/constant/ExceptionCodes.java
@@ -6,4 +6,6 @@ public class ExceptionCodes {
     public static final String FILE_SRV_COULD_NOT_CREATE_DIRECTORY = "Could not create the directory where the uploaded files will be stored.";
     public static final String FILE_SRV_INVALID_FILE_NAME = "Sorry! Filename contains invalid path sequence ";
     public static final String FILE_SRV_NOT_FOUND = "File not found ";
+
+    public static final String FILE_CNT_COULD_NOT_DETERMINE_FILE_TYPE = "Could not determine file type.";
 }
--- src/main/java/ir/ssatari/fileservice/controller/FileController.java
+++ src/main/java/ir/ssatari/fileservice/controller/FileController.java
@@ -1,14 +1,19 @@
 package ir.ssatari.fileservice.controller;
 
+import ir.ssatari.fileservice.constant.ExceptionCodes;
 import ir.ssatari.fileservice.controller.response.UploadFileResponse;
 import ir.ssatari.fileservice.service.FileStorageService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -43,5 +48,23 @@ public class FileController {
         return Arrays.stream(files)
                 .map(this::uploadFile)
                 .collect(Collectors.toList());
+    }
+
+    @GetMapping("/downloadFile/{fileName:.+}")
+    public ResponseEntity<Resource> downloadFile(@PathVariable String fileName, HttpServletRequest request) {
+        Resource resource = fileStorageService.loadFileAsResource(fileName);
+        String contentType = null;
+        try {
+            contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+        } catch (IOException ex) {
+            log.info(ExceptionCodes.FILE_CNT_COULD_NOT_DETERMINE_FILE_TYPE);
+        }
+        if (contentType == null) {
+            contentType = "application/octet-stream";
+        }
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(contentType))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+                .body(resource);
     }
 }
