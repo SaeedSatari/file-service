094df4973ce14d2ea4b4e57cd00bf0a1886983ec
SaeedSatari <saeedsatari93@gmail.com>
2019-05-28 17:42:48 +0430

    initial commit

---
+++ src/main/java/ir/ssatari/fileservice/FileServiceApplication.java
@@ -0,0 +1,16 @@
+package ir.ssatari.fileservice;
+
+import ir.ssatari.fileservice.property.FileStorageProperties;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@SpringBootApplication
+@EnableConfigurationProperties({FileStorageProperties.class})
+public class FileServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileServiceApplication.class, args);
+    }
+
+}
